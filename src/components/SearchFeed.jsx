dfda036f760d4a7de1501cd832bdcf5d1f672346
aakanshakansal <aakanshakansal56@gmail.com>
2024-03-01 23:04:01 +0530

    fourth commit

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -17,7 +17,7 @@ const SearchFeed = () => {
   return (
     <Box p={2} minHeight="95vh">
       <Typography variant="h4" fontWeight={900}  color="white" mb={3} ml={{ sm: "100px"}}>
-        Search Results for <span style={{ color: "#fc1503" }}>{searchTerm}</span> videos
+        Search Results for <span style={{ color: "#3443eb" }}>{searchTerm}</span> videos
       </Typography>
       <Box display="flex">
         <Box sx={{ mr: { sm: '100px' } }}/>
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -16,7 +16,6 @@ export const logo = 'favicon-32x32.png';
 
 export const categories = [
   { name: 'New', icon: <HomeIcon />, },
-  { name: 'JS Mastery', icon: <CodeIcon />, },
   { name: 'Coding', icon: <CodeIcon />, },
   { name: 'ReactJS', icon: <CodeIcon />, },
   { name: 'NextJS', icon: <CodeIcon />, },
